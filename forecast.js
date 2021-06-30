043da53ec19380222b94eac345e305c711e2688a
05615 <74238913+05615@users.noreply.github.com>
2021-06-30 16:47:06 +0100

    Create forecast.js

---
+++ forecast.js
@@ -0,0 +1,21 @@
+//allows for the user to change between celsius and fahrenheit
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
+
+// when the user clicks on the temperature on the display
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.value === undefined) return;
+    
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+        
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius"
+    }
+
+});
